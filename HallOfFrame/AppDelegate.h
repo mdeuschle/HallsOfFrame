c53b84058a73b31870b2e9ce9781abed1dbf6a89
Matt Deuschle <mdeuschle@gmail.com>
2016-01-20 14:36:45 -0600

    Initial Commit

---
+++ HallOfFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrame
+//
+//  Created by Matt Deuschle on 1/20/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
